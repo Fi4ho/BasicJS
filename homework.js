5d66cdfc080e33f7d01f0f046186ba57dafb2008
Fi4ho <153329881+Fi4ho@users.noreply.github.com>
2023-12-09 15:16:10 +0100

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,17 @@
+
+let a = prompt("Enter side numOne");
+let b = prompt("Enter side numTwo");
+
+
+
+
+let numOne = 30;
+let numTwo = 119.95;
+let numThree = 5;
+
+
+
+let sum = numOne * numTwo * numThree;
+console.log("The sum of the price of iphones is:");
+console.log(sum);
+
